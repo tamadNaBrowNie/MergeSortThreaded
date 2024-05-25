38ead08a44ccbe4c855621fa814a813ea5474072
tamadNaBrowNie <shawn_aberin@dlsu.edu.ph>
2024-05-25 16:20:28 +0800

    it works with a hacky ineffficient solution

--- Main.java
+++ Main.java
@@ -4,8 +4,9 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.*;
 import java.util.Optional;
+
 public class Main {
-    
+
     public static void main(String[] args) {
         int seed = 0;
         // TODO: Seed your randomizer
@@ -18,29 +19,31 @@ public class Main {
             System.out.println(arr[i]);
         }
         System.out.println();
-        // TODO: Generate a random array of given 
+        // TODO: Generate a random array of given
         // TODO: generate_intervals but threadedp
-        List<Interval> intervals = generate_intervals(0, arr.length-1);
+        List<Interval> intervals = generate_intervals(0, arr.length - 1);
         ArrayList<Task> tasks = new ArrayList<Task>();
-        int threads = scanner.nextInt() ;
-        if(threads== 1)
-            intervals.forEach((c)->merge(arr,c.getStart(),c.getEnd()));
-        else{
-            new Task(intervals.get(intervals.size()-1),intervals,tasks,arr);
+        int threads = scanner.nextInt();
+        if (threads == 1)
+            intervals.forEach((c) -> merge(arr, c.getStart(), c.getEnd()));
+        else {
+            new Task(intervals.get(intervals.size() - 1), intervals, tasks, arr);
             try {
                 ExecutorService pool = Executors.newFixedThreadPool(threads);
-                tasks.forEach(i -> pool.execute(i));
+                do {
+                    tasks.forEach(i -> pool.execute(i));
+                } while (tasks.stream().anyMatch(task -> task.isDone() == false));
                 pool.shutdown();
-    
+
                 while (!pool.awaitTermination(0, TimeUnit.MICROSECONDS)) {
-    
+
                 }
             } catch (InterruptedException e) {
                 System.err.println("Exec interrupted");
             }
         }
         System.out.println();
-        for(int i:arr){
+        for (int i : arr) {
             System.out.println(i);
         }
         scanner.close();
@@ -68,7 +71,7 @@ public class Main {
         frontier.add(new Interval(start, end));
 
         int i = 0;
-        for (;i < frontier.size();i++) {
+        for (; i < frontier.size(); i++) {
             int s = frontier.get(i).getStart();
             int e = frontier.get(i).getEnd();
 
@@ -139,19 +142,22 @@ class Interval {
     private int start;
     private int end;
 
-
     public Interval(int start, int end) {
         this.start = start;
         this.end = end;
     }
 
-    public int getStart () {return this.start;}
+    public int getStart() {
+        return this.start;
+    }
 
     public void setStart(int start) {
         this.start = start;
     }
 
-    public int getEnd(){ return this.end;}
+    public int getEnd() {
+        return this.end;
+    }
 
     public void setEnd(int end) {
         this.end = end;
@@ -159,58 +165,59 @@ class Interval {
 }
 
 // Prefer composition over inheritance.
-class Task implements Runnable{
+class Task implements Runnable {
     private Interval interval;
     private Task l_child, r_child;
-    private boolean done=false;
+    private boolean done = false;
     private int[] array;
-    public boolean isDone(){return done;}
 
-    public void run(){
+    public boolean isDone() {
+        return done;
+    }
+
+    public void run() {
         if (done)
             return;
-        boolean left = (l_child == null)?true:l_child.isDone() ,
-        right = (r_child == null)?true:r_child.isDone();
-        System.out.printf("\n%d %d\n",this.interval.getStart(),this.interval.getEnd());
-        if(left&&right){
-           
-            // System.out.printf("\n%d %d %d %d\n",this.r_child.interval.getStart(),this.r_child.interval.getEnd(),this.l_child.interval.getStart(),this.l_child.interval.getEnd());
-            Main.merge(array,interval.getStart(),interval.getEnd());
-            done=true;
+        boolean left = (l_child == null) ? true : l_child.isDone(),
+                right = (r_child == null) ? true : r_child.isDone();
+        // System.out.printf("\n%d %d\n", this.interval.getStart(), this.interval.getEnd());
+        if (left && right) {
+
+            // System.out.printf("\n%d %d %d
+            // %d\n",this.r_child.interval.getStart(),this.r_child.interval.getEnd(),this.l_child.interval.getStart(),this.l_child.interval.getEnd());
+            Main.merge(array, interval.getStart(), interval.getEnd());
+            done = true;
 
         }
-        for (int i:array)System.out.println(i);
-        System.out.println();
+        // for (int i : array)
+        //     System.out.println(i);
+        // System.out.println();
     }
-    // TODO:RECURSIVE STATE CONSTRUCTOR THAT LOOKS FOR ITS CHILDREN. hell make it create Tasks
-    
-    Task(Interval i, List<Interval> list, List<Task> tasks,int[] arr ){
+    // TODO:RECURSIVE STATE CONSTRUCTOR THAT LOOKS FOR ITS CHILDREN. hell make it
+    // create Tasks
+
+    Task(Interval i, List<Interval> list, List<Task> tasks, int[] arr) {
         interval = i;
         this.array = arr;
         tasks.add(this);
-        if(i.getStart() == i.getEnd()){
-            r_child=l_child = null;
+        if (i.getStart() == i.getEnd()) {
+            r_child = l_child = null;
             return;
         }
-        Interval left =list.stream()
-                            .filter(
-                                child->child.getStart() == i.getStart()
-                                 && child.getEnd() 
-                                 == (i.getStart()+i.getEnd())>>1)
-                                 .findFirst()
-                                 .orElse(null);
-            Interval right =list.stream()
-                            .filter(child->child.getEnd() == i.getEnd()
-                             && child.getStart() 
-                             == (
-                                (i.getStart()+i.getEnd())>>1)+1)
-                                .findFirst()
-                                .orElse(null);
-            l_child=new Task(left,list,tasks,arr);
-            r_child=new Task(right,list,tasks,arr);
-
+        Interval left = list.stream()
+                .filter(
+                        child -> child.getStart() == i.getStart()
+                                && child.getEnd() == (i.getStart() + i.getEnd()) >> 1)
+                .findFirst()
+                .orElse(null);
+        Interval right = list.stream()
+                .filter(child -> child.getEnd() == i.getEnd()
+                        && child.getStart() == ((i.getStart() + i.getEnd()) >> 1) + 1)
+                .findFirst()
+                .orElse(null);
+        l_child = new Task(left, list, tasks, arr);
+        r_child = new Task(right, list, tasks, arr);
 
     }
 
-    
 }
